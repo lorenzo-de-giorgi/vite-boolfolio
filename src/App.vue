ff2e7ad6556fb7af44177af9ea256463de9cf621
lorenzo-de-giorgi <lorenzodegiorgi2004@gmail.com>
2024-06-10 15:19:29 +0200

    completed milestone 3 & 4

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,34 @@
 <template>
   <div>
     <h1>ciao</h1>
-    <i class="fa fa-solid fa-home"></i>
+    <ul>
+      <li v-for="post in posts" :key="projects.id">{{ projects.title }}</li>
+    </ul>
   </div>
 </template>
 
 <script>
+import axios from 'axios';
+import { store } from './store';
   export default {
-    
+    name: 'App',
+    data() {
+      return {
+        store,
+        projects: []
+      }
+    },
+    methods: {
+      getAllProjects() {
+        axios.get(this.store.apiBaseURL+'/projects').then((res)=>{
+          console.log(res.data);
+          this.projects = res.data.results;
+        });
+      }
+    },
+    mounted(){
+      this.getAllProjects();
+    }
   }
 </script>
 
