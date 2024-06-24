8383871567f9465d95e8b71b10ecc3047a19f7ef
lorenzo-de-giorgi <lorenzodegiorgi2004@gmail.com>
2024-06-24 17:57:47 +0200

    added typing effect on h1

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -1,17 +1,20 @@
 <template>
     <header>
         <nav class="navbar navbar-expand-lg bg-body-tertiary">
-            <div class="container-fluid">
-                <router-link to="/" class="navbar-brand">
-                    <i class="fa-solid fa-store"></i>MyPortfolio
-                </router-link>
-
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
-                    aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation"
-                    @click="show = !show">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse" :class="{ 'show': show }" id="navbarNav">
+            <div class="container-fluid d-flex">
+                <div>
+                    <router-link to="/" class="navbar-brand">
+                        LDG
+                    </router-link>
+                </div>
+                <div>
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+                        aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation"
+                        @click="show = !show">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                </div>  
+                <div class="collapse navbar-collapse ms-2" :class="{ 'show': show }" id="navbarNav">
                     <ul class="navbar-nav">
                         <li class="nav-item" v-for="(link, index) in menuItems" :key="index">
                             <!-- <a class="nav-link active" aria-current="page" href="#">Home</a> -->
@@ -23,7 +26,7 @@
                     </ul>
                     <div class="ms-auto">
                         <a href="http://localhost:8000" class="nav-link">
-                            <i class="fa-solid fa-user"></i> <span>Area Riservata</span>
+                            <i class="fa-solid fa-user fs-3"></i>
                         </a>
                     </div>
                 </div>
--- src/pages/AppHome.vue
+++ src/pages/AppHome.vue
@@ -1,6 +1,6 @@
 <template>
-    <div>
-        <h1 class="mt-5 mb-3">Homepage</h1>
+    <div class="d-flex justify-content-center align-items-center">
+        <h1 class="typing-demo">Lorenzo De Giorgi</h1>
     </div>
 </template>
 
@@ -11,5 +11,26 @@
 </script>
 
 <style lang="scss" scoped>
+.typing-demo {
+  width: 17ch;
+  animation: typing 2s steps(13), blink .5s step-end infinite alternate;
+  white-space: nowrap;
+  overflow: hidden;
+  border-right: 3px solid;
+  font-family: monospace;
+  font-size: 2em;
+}
+
+@keyframes typing {
+  from {
+    width: 0
+  }
+}
+    
+@keyframes blink {
+  50% {
+    border-color: transparent
+  }
+}
 
 </style>
